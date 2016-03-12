81a2e3cb558c05ed29395e083f642f45e69eb101
rootven <rutven.cat@gmail.com>
2016-03-12 23:55:15 +0300

    6

--- web/ask/ask/urls.py
+++ web/ask/ask/urls.py
@@ -18,10 +18,10 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', 'qa.views.test', name='home'),
-    url(r'login/.*/$)', 'qa.views.test', name='login'),
-    url(r'signup/.*/$', 'qa.views.test', name='signup'),
-    url(r'question/(\d+)/$', 'qa.views.test', name='question'),
-    url(r'ask/.*/$', 'qa.views.test', name='ask'),
-    url(r'popular/.*/$', 'qa.views.test', name='popular'),
-    url(r'new/.*/$', 'qa.views.test', name='new'),
+    url(r'^login/', 'qa.views.test', name='login'),
+    url(r'^signup/', 'qa.views.test', name='signup'),
+    url(r'^question/(\d+)/', 'qa.views.test', name='question'),
+    url(r'^ask/', 'qa.views.test', name='ask'),
+    url(r'^popular/', 'qa.views.test', name='popular'),
+    url(r'^new/', 'qa.views.test', name='new'),
 ]
